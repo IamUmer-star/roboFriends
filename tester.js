36962746ddbffdf6ce457dd1d3b9d2f439d960a1
Shabab Qaisar <shababq@themoonshotfactory.com>
2022-04-05 13:50:21 +0500

    Added Ci/Cd in this projec10 added url testing

--- tester.js
+++ tester.js
@@ -1,57 +1,49 @@
-const { exec, } = require("child_process");
+const { exec } = require("child_process");
 const axios = require("axios");
 
-class GracefullyExit extends Error {  
-    constructor (message) {
-      super(message)
-  
-      // assign the error class name in your custom error (as a shortcut)
-      this.name = this.constructor.name
-  
-      // capturing the stack trace keeps the reference to your error class
-      Error.captureStackTrace(this, this.constructor);
-    }
+class GracefullyExit extends Error {
+  constructor(message) {
+    super(message);
+
+    // assign the error class name in your custom error (as a shortcut)
+    this.name = this.constructor.name;
+
+    // capturing the stack trace keeps the reference to your error class
+    Error.captureStackTrace(this, this.constructor);
   }
-  
+}
 
 exec("npm start", (error, stdout, stderr) => {
-    console.log("Exited gracefully")
+  console.log("Exited gracefully");
 });
 
-async function checkIfServerIsLive(){
-    try{
-        const result = await axios.get("http://localhost:3000");
-        return true;
-    }catch (e){
-        // console.log(e);
-        return false;
-    }
+async function checkIfServerIsLive() {
+  try {
+    const result = await axios.get("http://localhost:3000");
+    return true;
+  } catch (e) {
+    // console.log(e);
+    return false;
+  }
 }
 
-
-async function startTesting(){
-    let isServerUp = false;
-    while(!isServerUp){
-        isServerUp = await checkIfServerIsLive();
-        console.log({isServerUp})
+async function startTesting() {
+  let isServerUp = false;
+  while (!isServerUp) {
+    isServerUp = await checkIfServerIsLive();
+    if (isServerUp) {
+      console.log("server is up in background Start Testing");
     }
-    exec("npx playwright test", (error, stdout, stderr) => {
-        if(error){
-            console.error(error);
-            return;
-        }
-        console.log("exiting by process.exit")
-        process.exit()
-    });
+  }
+  exec("npx playwright test", (error, stdout, stderr) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(stdout);
+    console.log("exiting by process.exit");
+    process.exit();
+  });
 }
 
-(async () =>{
-    try{
-        await startTesting()
-    }catch(e){
-        console.log(e)
-        // if(e instanceof GracefullyExit){
-        //     // console.error(e);
-        // }else {console.error(e)}
-    }
-})()
+(async () => {try {await startTesting();} catch (e) {console.log(e);}})();
--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,19 +1,29 @@
 const { test, expect } = require('@playwright/test');
 
 
-test.beforeEach(async ({ page }) => {
-    await page.goto('localhost:3000');
-  });
-
   test.describe('first test' , ()=>{
+   
 
     test('cheking h1 have text robofriends test' , async ({page})=>{
+      await page.goto('localhost:3000');
         await expect(page.locator('.test-driver')).toHaveText('roboFriends')
     })
 
-    test('checking ', async ({page})=>{
-        await expect(page.locator('.test-driver')).toHaveText('roboFriends')
-    })
+  })
 
+  test.describe("test for url checking" , async ()=>{
+
+    test("url is /counterpage" , async ({page})=>{
+      await page.goto('localhost:3000/counterpage');
+      
+      const button = await page.locator('test-button').click();
+      await expect(button).toBeTruthy()
+      
+    });
+
+    test("Urls testing" ,async ({page})=>{
+      expect(page.url()).toBe('http://localhost:3000/counterpage');
+
+    })
   })
 
