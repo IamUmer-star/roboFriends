8364536582fb0868542ba5ecc3b1749ef60053d2
Shabab Qaisar <shababq@themoonshotfactory.com>
2022-04-05 14:10:32 +0500

    Added Ci/Cd in this projec10 added url testin

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,29 +1,20 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
+test.describe("first test", () => {
+  test("cheking h1 have text robofriends test", async ({ page }) => {
+    await page.goto("localhost:3000");
+    await expect(page.locator(".test-driver")).toHaveText("roboFriends");
+  });
+});
 
-  test.describe('first test' , ()=>{
-   
-
-    test('cheking h1 have text robofriends test' , async ({page})=>{
-      await page.goto('localhost:3000');
-        await expect(page.locator('.test-driver')).toHaveText('roboFriends')
-    })
+test.describe("test for url checking", async () => {
+  test("url is /counterpage", async ({ page }) => {
+    await page.goto("localhost:3000/counterpage");
+    await expect(page.locator(".test-button")).toHaveText("Click")
+  });
+  test("is url /counterpage",async ({page}) =>{
+    await page.goto("localhost:3000/counterpage");
+    await expect(page.url()).toBe("http://localhost:3000/counterpage")
 
   })
-
-  test.describe("test for url checking" , async ()=>{
-
-    test("url is /counterpage" , async ({page})=>{
-      await page.goto('localhost:3000/counterpage');
-      
-      const button = await page.locator('test-button').click();
-      await expect(button).toBeTruthy()
-      
-    });
-
-    test("Urls testing" ,async ({page})=>{
-      expect(page.url()).toBe('http://localhost:3000/counterpage');
-
-    })
-  })
-
+});
